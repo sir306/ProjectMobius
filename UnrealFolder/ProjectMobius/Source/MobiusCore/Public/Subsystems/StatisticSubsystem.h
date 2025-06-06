da08619f84ab5b0973b0d7a723035a1982a664ef
Nicholas Harding <40708936+sir306@users.noreply.github.com>
2025-06-06 16:51:55 +1200

    created a blank subsystem
    currently, this subsystem is blank and will be used to pull logic from the floor stats widget, and have the widget access this subsystem for data to be displayed

---
+++ UnrealFolder/ProjectMobius/Source/MobiusCore/Public/Subsystems/StatisticSubsystem.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "StatisticSubsystem.generated.h"
+
+/** TODO: this will be used to pull logic from the heatmap subsystem and the floor stats widget so only one subsystem is used
+ * and provide a way to break one of the circular dependencies
+ * This subsystem will be used to perform calculations and statistics gathering for user interfaces
+ */
+UCLASS()
+class MOBIUSCORE_API UStatisticSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+};
