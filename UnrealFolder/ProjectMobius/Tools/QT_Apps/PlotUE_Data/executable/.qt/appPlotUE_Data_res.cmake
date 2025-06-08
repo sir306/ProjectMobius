8b8a3e3ae0d302bdb299420f1a5792ff6e13123d
Nicholas Harding <40708936+sir306@users.noreply.github.com>
2025-06-09 10:26:09 +1200

    install check
    Performed another install check on a new pc.
    
    TODO: need to update git ignore to exclude files that are caches etc

--- UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/executable/.qt/appPlotUE_Data_res.cmake
+++ UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/executable/.qt/appPlotUE_Data_res.cmake
@@ -1,11 +1,11 @@
 
 set(target "appPlotUE_Data")
-set(working_dir "D:/1_Work/MobiusPublic/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data")
+set(working_dir "E:/00_Work/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data")
 set(src_and_dest_list
-    "D:/1_Work/MobiusPublic/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/Shaders/PlaybarShaderEffect.frag"
-    "D:/1_Work/MobiusPublic/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/build/Desktop_Qt_6_9_0_MinGW_64_bit-release/PlotUE_Data/shaders/PlaybarShaderEffect.frag"
-    "D:/1_Work/MobiusPublic/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/Shaders/PlaybarShaderEffect.frag.qsb"
-    "D:/1_Work/MobiusPublic/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/build/Desktop_Qt_6_9_0_MinGW_64_bit-release/PlotUE_Data/shaders/PlaybarShaderEffect.frag.qsb"
+    "E:/00_Work/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/Shaders/PlaybarShaderEffect.frag"
+    "E:/00_Work/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/build/Desktop_Qt_6_9_0_MinGW_64_bit-release/PlotUE_Data/shaders/PlaybarShaderEffect.frag"
+    "E:/00_Work/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/Shaders/PlaybarShaderEffect.frag.qsb"
+    "E:/00_Work/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/build/Desktop_Qt_6_9_0_MinGW_64_bit-release/PlotUE_Data/shaders/PlaybarShaderEffect.frag.qsb"
 
 )
-set(timestamp_file "D:/1_Work/MobiusPublic/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/build/Desktop_Qt_6_9_0_MinGW_64_bit-release/.qt/appPlotUE_Data_res.txt")
+set(timestamp_file "E:/00_Work/ProjectMobius/UnrealFolder/ProjectMobius/Tools/QT_Apps/PlotUE_Data/build/Desktop_Qt_6_9_0_MinGW_64_bit-release/.qt/appPlotUE_Data_res.txt")
